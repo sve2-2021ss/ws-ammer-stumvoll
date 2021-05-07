db42186a1ac2877394ae0570362607a1ac7933b0
Simon Ammer <ammersimon@gmail.com>
2021-05-07 09:18:56 +0200

    Add client starter project

---
+++ client/statusdeck/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Simon Ammer on 07.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ client/statusdeck/Shared/statusdeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  statusdeckApp.swift
+//  Shared
+//
+//  Created by Simon Ammer on 07.05.21.
+//
+
+import SwiftUI
+
+@main
+struct statusdeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
