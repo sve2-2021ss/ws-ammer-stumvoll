fdc063e41a58153470d6ba378ba4a55c2956bca4
Simon Ammer <ammersimon@gmail.com>
2021-05-10 13:46:30 +0200

    Add ApolloCodegen

--- client/statusdeck/BuildTools/Package.swift
+++ client/statusdeck/BuildTools/Package.swift
@@ -5,8 +5,7 @@ let package = Package(
     name: "BuildTools",
     platforms: [.macOS(.v10_11)],
     dependencies: [
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.41.2"),
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.1"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.41.2")
     ],
     targets: [.target(name: "BuildTools", path: "")]
 )
