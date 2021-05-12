31c026fdd123c812bbb25fe94fb7e683536999f5
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-05-12 13:06:27 +0200

    server: add loaders to context

--- server/src/api/Context.ts
+++ server/src/api/Context.ts
@@ -1,11 +1,14 @@
-import {jobsByPipelineIdLoader} from "./dataloaders/jobLoaders"
+import {jobsByIdLoader, jobsByPipelineIdLoader} from "./dataloaders/jobLoaders"
 import {changesByJobIdLoader} from "./dataloaders/changeLoaders"
 import {stagesByJobIdLoader} from "./dataloaders/stageLoaders"
 import {userByIdLoader} from "./dataloaders/userLoaders"
+import {pipelineByIdLoader} from "./dataloaders/pipelineLoaders";
 
 export const createContext = () => ({
     loaders: {
+        pipelinesById: pipelineByIdLoader(),
         jobsByPipelineId: jobsByPipelineIdLoader(),
+        jobsById: jobsByIdLoader(),
         changesByJobId: changesByJobIdLoader(),
         stagesByJobId: stagesByJobIdLoader(),
         usersById: userByIdLoader()
