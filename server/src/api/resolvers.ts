1f00cc6f221d72d7a00c6e736ae5490f93c19b61
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-05-12 13:06:38 +0200

    server: implement resolver logic

--- server/src/api/resolvers.ts
+++ server/src/api/resolvers.ts
@@ -10,12 +10,15 @@ import {Change} from "./types/Change"
 // noinspection JSUnusedGlobalSymbols
 export const resolvers: IResolvers<any, Context> = {
     Pipeline: {
-        jobs: (pipeline: Pipeline, _, {loaders}): Promise<Job[]> => loaders.jobsByPipelineId.load(pipeline.id)
+        jobs: (pipeline: Pipeline, _, {loaders}): Promise<Job[]> => loaders.jobsByPipelineId.load(pipeline.id),
     },
     Job: {
         changes: (job: Job, _, {loaders}) => loaders.changesByJobId.load(job.id),
         stages: (job: Job, _, {loaders}) => loaders.stagesByJobId.load(job.id),
         issuer: (job: Job, _, {loaders}) => loaders.usersById.load(job.issuerId)
+    },
+    JobById: {
+        pipeline: (job: Job, _, {loaders}) => loaders.pipelinesById.load(job.pipelineId)
     },
     State,
     Stage: {},
@@ -24,11 +27,8 @@ export const resolvers: IResolvers<any, Context> = {
         changer: (change: Change, _, {loaders}) => loaders.usersById.load(change.changerId)
     },
     Query: {
-        pipeline(_, {id}): Promise<Pipeline | undefined> {
-            return Container.get(PipelineService).getById(id)
-        },
-        pipelines(): Promise<Pipeline[]> {
-            return Container.get(PipelineService).getAll()
-        }
+        pipeline: (_, {id}): Promise<Pipeline | undefined> => Container.get(PipelineService).getById(Number(id)),
+        pipelines: (): Promise<Pipeline[]> => Container.get(PipelineService).getAll(),
+        job: (pipeline: Pipeline, {id}, {loaders}): Promise<Job | undefined> => loaders.jobsById.load(Number(id))
     },
 }
